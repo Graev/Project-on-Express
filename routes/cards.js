4e03860a52340304cdf3f089a44628907626792f
Konstantin Graev <graev.k@gmail.com>
2019-11-07 15:10:47 +0300

    Error correction
    Справил метод подключение JSON файлов.
    Изменил метод поиска пользователя по id.

--- routes/cards.js
+++ routes/cards.js
@@ -1,15 +1,9 @@
 const routerCards = require('express').Router();
 const path = require('path');
-const fs = require('fs');
+const cardsJSON = require(path.join(__dirname, '..', 'data', 'cards.json'));
 
 routerCards.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, '..', 'data', 'cards.json'), { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.json(data);
-  });
+  res.send(cardsJSON);
 });
 
 module.exports = routerCards;
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,19 @@
 const routerUsers = require('express').Router();
 const path = require('path');
-const fs = require('fs');
+const userJSON = require(path.join(__dirname, '..', 'data', 'users.json'));
 
 routerUsers.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
+  res.send(userJSON);
 });
 
 routerUsers.get('/:id', (req, res) => {
-  fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const user = JSON.parse(data).filter((i) => {
-      if (i._id == req.params.id) {
-        return true;
-      }
-      return false;
-    });
+  const user = userJSON.find((i) => i._id === req.params.id);
 
-    if (user.length === 0) {
-      res.send({ "message": "Нет пользователя с таким id" });
-      return;
-    }
-    res.send(user);
-  });
+  if (user === undefined) {
+    res.send({ "message": "Нет пользователя с таким id" });
+    return;
+  }
+  res.send(user);
 });
 
 module.exports = routerUsers;
